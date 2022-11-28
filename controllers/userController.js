78784a90d20ca572962171a20c934f6cc73ac875
BadMonkeyDev <arsenii.aivazian@gmail.com>
2022-11-28 15:37:31 +0200

    JWT Registration and Login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,55 @@
 const ApiError = require("../error/ApiError")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const {User, Basket} = require("../models/models")
+
+const generateJwt = (id, email, role) => {
+    return jwt.sign(
+        {id, email, role},
+        process.env.SECRET_KEY,
+        {expiresIn: "24h"}
+    )
+}
 
 class UserController {
-    async registration(req, res) {
+    async registration(req, res, next) {
+        const {email, password, role} = req.body
 
+        if (!email || !password) {
+            return next(ApiError.badRequest("Неправильна електронна пошта або пароль"))
+        }
+
+        const candidate = await User.findOne({where: {email}})
+
+        if (candidate){
+            return next(ApiError.badRequest("Користувач з такою електорнною поштою вже зареєстрований!"))
+        }
+
+        const hashPassword = await bcrypt.hash(password, 5)
+
+        const user = await User.create({
+            email,
+            role,
+            password: hashPassword
+        })
+
+        const basket = await Basket.create({userId: user.id})
+        const token = generateJwt(user.id, user.email, user.role)
+        return res.json({token})
     }
 
-    async login(req, res) {
-
+    async login(req, res, next) {
+        const {email, password} = req.body
+        const user = await User.findOne({where: {email}})
+        if (!user) {
+            return next(ApiError.internal("Користувача з такими даними не знайдено!"))
+        }
+        let comparePassword = bcrypt.compareSync(password, user.password)
+        if (!comparePassword) {
+            return next(ApiError.internal("Неправильний пароль!"))
+        }
+        const token = generateJwt(user.id, user.email, user.role)
+        return res.json({token})
     }
 
     async checkAuth(req, res, next) {
